80e337f8b43c6af8dbcaf1cefb79a7b39096415d
Flustivs <rune170206@gmail.com>
2023-09-08 12:34:01 +0200

    ZBC

---
+++ ArduinoData/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO.Ports;
+using System.IO;
+namespace ArduinoSerialToFile
+{
+    class Program
+    {
+        //Global variabler
+        static string previousSentence = null;
+        static byte count = 0;
+        static string data = "";
+
+        static void Main(string[] args)
+        {
+            // Hvilken Port og dens bits i sekundet
+            SerialPort serialPort = new SerialPort("COM3", 9600);
+            // Den her lytter på på dens representive method
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialPortDataReceived);
+
+            serialPort.Open();
+            Console.WriteLine("Listening on " + serialPort.PortName);
+            Console.ReadKey();
+            serialPort.Close();
+        }
+        private static void SerialPortDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //Try reading my arduinos data and put it in a file
+            try
+            {
+                SerialPort sp = (SerialPort)sender;
+                data = sp.ReadLine();
+                Console.WriteLine(data);
+
+                
+                // Looking for 2 Clap
+                /*
+                if (data.Equals(previousSentence, StringComparison.OrdinalIgnoreCase))
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 0;
+                    previousSentence = data;
+                }
+                */
+
+                // Gemme data i en tekstfil
+                using (StreamWriter sw = new StreamWriter("C:\\Users\\rune1\\Desktop\\output.txt", true))
+                {
+                    sw.WriteLine(data);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: " +  ex.Message);
+            }
+        }
+        // A bad try on making something
+        /* 
+        public void HTML()
+        {
+            byte Blinds = 0;
+            byte clap = 0;
+            byte light = 0;
+
+            if (Blinds == 2)
+            {
+                //over i HTML og ændre billedet
+            }
+            else
+            {
+
+            }
+        }*/
+    }
+}
---
+++ Clap Webs/Pages/Index.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Clap_Webs.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        //my Filepath
+        string filePath = @"C:\Users\rune1\Desktop\output.txt";
+
+        public int Counter { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            Counter = CountClaps(LoadFile(filePath));
+        }
+
+        //Here we count the claps from the string made from LoadFile
+        public IActionResult OnGetGetClapCount()
+        {
+            Counter = CountClaps(LoadFile(filePath));
+            return new JsonResult(new { count = Counter });
+        }
+        //We convert the file to a string
+        private string LoadFile(string filePath)
+        {
+            StringBuilder fileContents = new StringBuilder();
+            using (var sr = new StreamReader(filePath))
+            {
+                fileContents.Append(sr.ReadToEnd());
+            }
+
+            return fileContents.ToString();
+        }
+        //We count the amount of claps and split and lay it in an array
+        private int CountClaps(string content)
+        {
+            string[] claps = content.Split(new char[] { ' ','\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] clapArray = claps.Where(word => string.Equals(word, "clap", StringComparison.OrdinalIgnoreCase)).ToArray();
+            return clapArray.Length;
+        }
+    }
+}

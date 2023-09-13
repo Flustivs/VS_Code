2b0e09a74b96f0cf8a66d25179cc21019983bc44
Flustivs <rune170206@gmail.com>
2023-09-13 09:09:07 +0200

    ZBC

---
+++ Fordelingshatten/SortingHat.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fordelingshatten
+{
+    public class SortingHat
+    {
+        enum Colleges { Griffindor, Hufflepuff, Ravenclaw, Slytherin };
+
+        public int Random()
+        {
+            Random random = new Random();
+            int num = random.Next(0,4);
+            return num;
+
+        }
+        public void College()
+        {
+            while(true)
+            {
+                int x = Random();
+                switch (x)
+                {
+                    case 0:
+                        Console.WriteLine(Colleges.Griffindor);
+                        break;
+                    case 1:
+                        Console.WriteLine(Colleges.Hufflepuff);
+                        break;
+                    case 2:
+                        Console.WriteLine(Colleges.Ravenclaw);
+                        break;
+                    case 3:
+                        Console.WriteLine(Colleges.Slytherin);
+                        break;
+                    default:
+                        Console.WriteLine("Your not worht it");
+                        break;
+
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}

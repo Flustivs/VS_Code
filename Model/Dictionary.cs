1a8e42ebd120b6e43ab091756a1c14748985a737
Flustivs <rune170206@gmail.com>
2023-09-16 16:27:27 +0200

    Add project files.

---
+++ Model/Dictionary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jedi_Mind.Model
+{
+    public class Dictionary
+    {
+        Dictionary<string, int> _dictionary = new Dictionary<string, int>();
+
+
+        public Dictionary<string, int> AddValues()
+        {
+            if (!_dictionary.Any())
+            {
+                _dictionary.Add("Luke", 37);
+                _dictionary["DarthVader"] = 29;
+                _dictionary.Add("Han", 14);
+            }
+            return _dictionary;
+        }
+        public Dictionary<string, int> Remove()
+        {
+            _dictionary.Remove("Han");
+            return _dictionary;
+        }
+        public Dictionary<string, int> Remaining()
+        {
+            return _dictionary;
+        }
+    }
+}
---
+++ View/GUI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jedi_Mind.View
+{
+    public class GUI
+    {
+        public void AllValues(Dictionary<string,int> dictionary)
+        {
+            foreach(var par in dictionary)
+            {
+                Console.WriteLine($"Key: {par.Key}, Value: {par.Value}");
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public void Writefirst(Dictionary<string, int> dictionary)
+        {
+            var First = dictionary.First();
+            Console.WriteLine($"Key: {First.Key}, Value: {First.Value}");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public void Remove(Dictionary<string, int> dictionary)
+        {
+            Console.WriteLine("Removed: Han");
+            Console.ReadKey();Console.Clear();
+        }
+    }
+}
